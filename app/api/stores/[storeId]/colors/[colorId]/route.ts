05d313d901be045db5d605bfe2a28a713b1b0492
Pranay Jaulkar <pranaypj13@gmail.com>
2023-12-28 12:41:33 +0530

    added colors route handlers

---
+++ app/api/stores/[storeId]/colors/[colorId]/route.ts
@@ -0,0 +1,95 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { Color } from "@prisma/client";
+import cloudinary from "@/cloudinary";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { colorId: string } }
+) {
+  try {
+    if (!params.colorId) {
+      return new NextResponse("Color id is required", { status: 400 });
+    }
+    const color = await prismadb.color.findUnique({
+      where: { id: params.colorId },
+    });
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[Color_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { name, value } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 404 });
+    }
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+    if (!value) {
+      return new NextResponse("Value is required", { status: 400 });
+    }
+    if (!params.colorId) {
+      return new NextResponse("Color id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: { id: params.storeId, userId },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+    const updatedColor = await prismadb.color.update({
+      where: { id: params.colorId },
+      data: { name, value },
+    });
+    return NextResponse.json(updatedColor);
+  } catch (error) {
+    console.log("[Color_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 404 });
+    }
+    if (!params.colorId) {
+      return new NextResponse("Color id is required", { status: 400 });
+    }
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+    const storeByUserId = await prismadb.store.findFirst({
+      where: { id: params.storeId, userId },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+    const deletedColor = await prismadb.color.delete({
+      where: { id: params.colorId },
+    });
+    return NextResponse.json(deletedColor);
+  } catch (error) {
+    console.log("[Color_DELETE]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
