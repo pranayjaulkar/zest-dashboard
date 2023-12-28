b028354add85f6f80f48c72724f2da708fc89f04
Pranay Jaulkar <pranaypj13@gmail.com>
2023-12-28 12:32:13 +0530

    changes

---
+++ app/(dashboard)/[storeId]/billboards/(components)/Client.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus as PlusIcon } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { DataTable } from "@/components/ui/dataTable";
+import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./CellAction";
+import { Billboard } from "@prisma/client";
+import { useState } from "react";
+import { format } from "date-fns";
+import ApiList from "@/components/ui/apiList";
+
+export type BillboardColumn = {
+  id: string;
+  label: string;
+  createdAt: string;
+};
+
+interface BillboardClientProps {
+  billboards: Billboard[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+  billboards,
+}) => {
+  const router = useRouter();
+  const params = useParams();
+  const [data, setData] = useState<BillboardColumn[]>(
+    billboards.map((item) => ({
+      id: item.id,
+      label: item.label,
+      createdAt: format(item.createdAt, "dd-MM-yyy"),
+    }))
+  );
+
+  const columns: ColumnDef<BillboardColumn>[] = [
+    {
+      accessorKey: "label",
+      header: "Label",
+    },
+    {
+      accessorKey: "createdAt",
+      header: "Date",
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => <CellAction data={row.original} setData={setData} />,
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <PlusIcon className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading title="API" description="API calls for billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
+    </>
+  );
+};
+
+export default BillboardClient;
