08f698979bd115ff5adb3ce7fc0f05afe7aa6208
pranayjaulkar <pranaypj13@gmail.com>
2024-08-30 13:07:36 +0530

    added demo login email and password

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,24 @@
-
 function layout({ children }: { children: React.ReactNode }) {
+  const email = "pranay@gmail.com";
+  const password = "52s5&8ss55Bop";
+
   return (
-    <div className="flex items-center justify-center min-h-screen min-w-screen">{children}</div>
+    <div className="flex items-center justify-center min-w-screen">
+      <div className="flex space-y-8 flex-col mt-28">
+        <div className="flex flex-col space-y-2 w-full text-sm font-semibold">
+          <p className="text-xl font-semibold">Demo Email and Password for Login</p>
+          <div className="flex space-x-2 w-full">
+            <p>Email:</p>
+            <p>{email}</p>
+          </div>
+          <div className="flex space-x-2 w-full">
+            <p>Password:</p>
+            <p>{password}</p>
+          </div>
+        </div>
+        <div>{children}</div>
+      </div>
+    </div>
   );
 }
 
